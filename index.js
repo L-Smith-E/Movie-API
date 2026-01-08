0b99793d4ccf600ffc46d17196be30aa506176bc
Laurence Smith <33005711+L-Smith-E@users.noreply.github.com>
2026-01-07 21:57:50 -0500

    Add user endpoints and uuid dependency
    Introduced user-related API endpoints for creating, updating, retrieving, and deleting users and their favorite movies. Added the 'uuid' package for unique user IDs and updated 'body-parser' to version 2.2.2. Cleaned up redundant user endpoint definitions and improved route parameter naming for movie endpoints.

--- index.js
+++ index.js
@@ -18,9 +18,10 @@ let movies = [
     { title: 'Captain America: The Winter Soldier', director: 'Anthony and Joe Russo'},
     { title: '1917', director: 'Sam Mendes'}
 ];
+let users = [];
 
 app.use(express.static('public'));
-const accessLogStream = fs.createWriteStream(path.join(__dirname, 'public'), {flags: 'a'})
+//const accessLogStream = fs.createWriteStream(path.join(__dirname, 'public'), {flags: 'a'})
 
 // setup the logger
 app.use(morgan('combined'));
@@ -33,16 +34,57 @@ app.get('/secreturl', (req, res) => {
   res.send('This is a secret url with super top-secret content.');
 });
 
+
+//user endpoints
+//test - not for production
+app.get('/users', (req, res) => {
+    res.json(users);
+});
+app.post('/users', express.json(), (req, res) => {
+    let newUser = req.body;
+
+    if(!newUser.name){
+        const message = 'Missing name in request body';
+        res.status(400).send(message);
+    }
+    else{
+        newUser.id = uuid.v4();
+        users.push(newUser);
+        res.status(201).json(newUser);
+    }
+
+    res.status(201).json(newUser);
+});
+
+app.post('/users/:id/favourites/movies', express.json(), (req, res) => {
+    newFavourites = req.body;
+    res.status(201).json(newFavourites);
+});
+
+app.put('/users/:id/users', express.json(), (req, res) => {
+    res.send(`User with ID ${req.params.id} has updated their username to ${req.params.usernames}.`);
+});
+
+app.delete('/users/:id', (req, res) => {
+    res.send(`User with ID ${req.params.id} has been deleted.`);
+});
+app.delete('/users/:id/:favourites/:movies', (req, res) => {
+    res.send(`User with ID ${req.params.id} has deleted one of their favourite movie.`);
+});
+
+//movie endpoints
 app.get('/movies', (req, res) => {
     res.json(movies);
 });
 
-app.get('/movies/:director', (req, res) => {
+app.get('/movies/:directors', (req, res) => {
     const director = req.params.director;
+    res.send(`You requested movies directed by ${director}.`);
 });
 
 app.get('/movies/:title', (req, res) => {
     const title = req.params.title;
+    res.send(`You requested the movie "${title}".`);
 });
 
 app.get('/movies/:title/:genre', (req, res) => {
@@ -56,22 +98,6 @@ app.post('/movies', express.json(), (req, res) => {
     movies.push(newMovie);
     res.status(201).json(newMovie);
 });
-
-app.post('/users', express.json(), (req, res) => {
-    const newUser = req.body;
-    res.status(201).json(newUser);
-});
-
-app.post('/users/:id/:favourites', express.json(), (req, res) => {
-    newFavourites = req.body;
-    res.status(201).json(newFavourites);
-});
-
-app.put('/users/:id/:usernames', express.json(), (req, res) => {
-    res.send(`User with ID ${req.params.id} has updated their username to ${req.params.usernames}.`);
-});
-
-
 
 
 app.use((err, req, res, next) => {
