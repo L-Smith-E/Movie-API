600f4c405396659e8ddf55528b1cfbede61f72c2
Laurence Smith <33005711+L-Smith-E@users.noreply.github.com>
2026-02-02 14:09:56 -0500

    Use env PORT and add user password helpers
    Make the server port configurable via process.env.PORT and bind to 0.0.0.0 (useful for containers/hosts); update startup log to show the chosen port. Add User schema helpers: a static hashPassword(password) that bcrypt-hashes with 10 salt rounds, and an instance validatePassword(password) that compares a plain password to the stored hash.

--- index.js
+++ index.js
@@ -342,6 +342,7 @@ app.use((err, req, res, next) => {
   res.status(500).send('Something broke!: ' + err.message);
 });
 
-app.listen(8080, () => {
-  console.log('Server is running on http://localhost:8080');
+ const port = process.env.PORT || 8080;
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server is running on port ${port}`);
 });
--- models.js
+++ models.js
@@ -25,6 +25,15 @@ let userSchema = new mongoose.Schema({
     favoriteMovies: [{type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}]
 });
 
+userSchema.statics.hashPassword = (password) => {
+    return bcrypt.hashSync(password, 10);
+};
+
+userSchema.methods.validatePassword = function(password) {
+    return bcrypt.compareSync(password, this.password);
+};
+
+
 let Movie = mongoose.model('Movie', movieSchema);
 let User = mongoose.model('Users', userSchema);
 
